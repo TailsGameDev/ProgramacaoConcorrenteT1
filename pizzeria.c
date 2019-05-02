0763c7c2ddd5e926f7c9d4b389c52aa7a8505537
Caina <cainacc@hotmail.com>
2019-05-02 00:13:46 -0300

    alocacao de mesas e tchau garcom

--- pizzeria.c
+++ pizzeria.c
@@ -11,44 +11,82 @@
 //default:
 //INE5410_INFO=1 ./program 2 2 40 3 8 40 10
 
-sem_t sGarcons;
+sem_t sGarcons, sMesas, sLockMesas;
 
 pthread_mutex_t pegaFatia;
 
 int pizzariaAberta = 0; //famigerado True
 
+
 void pizzeria_init(int tam_forno, int n_pizzaiolos, int n_mesas, int n_garcons, int tam_deck, int n_grupos) {
   printf("chamou init");
   sem_init(&sGarcons, 0,n_garcons);
   pthread_mutex_init(&pegaFatia, NULL);
+
+  //para pegar as mesas:
+  sem_init(&sMesas, 0, n_mesas);
+  sem_init(&sLockMesas,0, 1); // decidi usar semaforo para que haja fila de clientes
+  //se fosse mutex, qualquer um poderia pegar quando liberasse. semaforo gera fila.
+
+
 }
 
 void pizzeria_close() {
+  //se pah a funcao eh soh isso:
+  pizzariaAberta = 1; //famigerado false
 }
 
 void pizzeria_destroy() {
   sem_destroy(&sGarcons);
   pthread_mutex_destroy(&pegaFatia);
+  sem_destroy(&sMesas);
+  sem_destroy(&sLockMesas);
 }
 
 void pizza_assada(pizza_t* pizza) {
 }
 
+int numMesas(int tam_grupo){
+  int numeroDeMesas = 0;
+  int tam = tam_grupo;
+  while (tam > 0){
+    numeroDeMesas++;
+    tam-=4;
+  }
+  return numeroDeMesas;
+}
+
 int pegar_mesas(int tam_grupo) {
+  int numeroDeMesas = numMesas(tam_grupo);
+  //printf("numeroDeMesas: %d; tam_grupo: %d\n", numeroDeMesas, tam_grupo); ok!
+
   if (pizzariaAberta) {
-    //TODO: logica de escolher mesas
+    /*Logica de escolher mesas
+    //
     //acho que o canale eh um grupo de clientes pegar mesas de cada vez,
     //dae ele pega todas as mesas que precisa e entao destrava pros outros
     //se usar 2 semaforos ao inves de um mutex e um semaforo, acho q fica
     //fila mais justa pros clientes
+    */
+    sem_wait(&sLockMesas);
+      if(!pizzariaAberta){ //caso a pizzaria fechou enquanto esperava
+        sem_post(&sLockMesas);
+        return -1;
+      }
+      //caso padrao eh alocar umas mesas
+      for (int i = numeroDeMesas; i > 0; i--){
+          sem_wait(&sMesas);
+      }
+    sem_post(&sLockMesas);
     return 0;
   }
   return -1;
 }
 
 void garcom_tchau(int tam_grupo) {
-  sem_post(&sGarcons);
-  //TODO: implementar logica de liberar as mesas
+  for(int i = 0; i<numMesas(tam_grupo); i++){
+    sem_post(&sGarcons);
+  }
 }
 
 void garcom_chamar() {
